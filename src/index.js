4d23c1d1298d82b6d1dfe9535f7706f42700b5a9
hyunm1n-o <hmoh0703@naver.com>
2025-10-08 01:28:57 +0900

    [week04] 실습코드 커밋

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// const express = require('express')  // -> CommonJS
+import express from 'express'          // -> ES Module
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Worldd!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
